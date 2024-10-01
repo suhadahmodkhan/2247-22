391ffd54cd2294bcac79d33241792afcfdf2bb41
suhadahmodkhan <suhadahmodkhan04@gmail.com>
2024-10-01 02:02:10 -0700

    ok

---
+++ evenOrOdd.js
@@ -0,0 +1,10 @@
+var i = 2208
+// console.log(i) 
+
+
+if(i % 2 == 0){
+    console.log( i  ," number is : even")
+}
+else {
+    console.log(i  ," number is : odd")
+}
---
+++ loop.js
@@ -0,0 +1,19 @@
+/*
+1.variable for control flow of loop
+
+*/
+var database = [1,2,3,4,5]
+
+var i = 101
+var condition = true
+while(condition){
+console.log("i love my country" , i)
+i = i + 1
+if(i == 110){
+    condition = false
+}
+}
+
+for(var j = 0 ; j < 100 ; j = j + 1 ){
+        console.log(j)
+}
